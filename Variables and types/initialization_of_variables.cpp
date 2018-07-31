2aa85f2b96125f3bcd19cf926b116a5a11dada3d
mahdi motamedi <mahdi.motamedi76@gmail.com>
2018-07-31 17:11:19 +0430

    modify initialization_of_variables.cpp

--- Variables and types/initialization_of_variables.cpp
+++ Variables and types/initialization_of_variables.cpp
@@ -1,14 +1,15 @@
 // initialization of variables
 
 #include <iostream>
+
 using namespace std;
 
-int main ()
+int main()
 {
-    int a=5;               // initial value: 5
-    int b(3);              // initial value: 3
-    int c{2};              // initial value: 2
-    int result;            // initial value undetermined
+    int a = 5;  // initial value: 5
+    int b(3);   // initial value: 3
+    int c{2};   // initial value: 2
+    int result; // initial value undetermined
 
     a = a + b;
     result = a - c;
