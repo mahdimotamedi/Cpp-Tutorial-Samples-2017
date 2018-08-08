a90821c17fdbd929efe1a0148fbc28e6b4139c4b
mahdi motamedi <mahdi.moatamedi@yahoo.com>
2018-08-08 23:01:18 +0430

    add some tabs and end lines

--- Some data types/enum_2.cpp
+++ Some data types/enum_2.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 enum seasons {
-    spring = 34, summer = 4, autumn = 9, winter = 32
+    spring = 34,
+    summer = 4,
+    autumn = 9,
+    winter = 32
 };
 
-int main()
-{
+int main() {
     seasons s;
 
     s = summer;
